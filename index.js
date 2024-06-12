2a6b45dcfc1106c9e2f0172928ec88478fef98f1
Timothy-Lu-8 <106774662+Timothy-Lu-8@users.noreply.github.com>
2024-06-11 23:35:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ require('dotenv').config()
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ["https://chinese-j0watp27a-timothy-lus-projects.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
 
 app.use('/api/characters', apiRoutes)
 mongoose.connect(process.env.MONGO_URI, )
